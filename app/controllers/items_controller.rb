21883e08eeb97ebb6a0df40605095747fe86ecf2
Kyle Maune <the.kyle.maune@gmail.com>
2017-12-21 14:24:27 -0700

    added/installed devise gem and generated user model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /items
   # GET /items.json
