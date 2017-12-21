7c5c9e17207e5694d1aa579817f8c4ab2b9f03de
Kyle Maune <the.kyle.maune@gmail.com>
2017-12-21 14:40:17 -0700

    Edited routes file for doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  use_doorkeeper
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications,
+      :authorized_applications
+  end
   devise_for :users
   root to: 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
